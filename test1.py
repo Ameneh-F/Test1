10ce34fd953d401e29c51b289044a111b4770052
Ameneh-F <amenehferasat2002@gmail.com>
2024-11-02 15:22:58 +0330

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,19 @@
+x = int(input("Enter a number : "))
+y = int(input("Enter a number : "))
+z = x * y
+print("The result is : ", z, sep = "")
+
+# This is my first exprience !
+""" This is my first exprience ! """
+
+# print(print.__doc__)
+
+sum = 0
+for i in range (5):
+    T = int(input("Enter a number : "))
+    sum = sum + T
+
+print(sum, end = "\n")
+print ("*************")
+    
+
